c4015fe51546c9378888b8a471ccfec6fa4fc49b
amyanderson2195 <143798592+amyanderson2195@users.noreply.github.com>
2023-09-14 14:48:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,8 +31,7 @@
                     <h1>Dashboard</h1>
                     <h3>Welcome back, Name.</h3>
                 </div>
-                <p>This has been adapted from a group project that I was involved with. I've removed the sidebar links to the pages created by other members of the team in order to showcase the parts that I worked on myself.</p>
-                <p>The original code and database was hosted on SETU's servers, but after summer exams in 2023, it became no longer possible to access it in full. I copied and edited my code from the Project Manual we submitted for the module, created a MySQL database with the necessary tables, and installed Apache 2.4 on my local machine in order to debug the PHP without needing to host it elsewhere.</p>
+                <p>This project uses HTML, PHP and JavaScript in conjunction with one another to access a MySQL database and perform Insert, Update and Delete actions through an easy-to-use interface. I have adapted it from a group project that I worked on in my second year of studying Software Development, showcasing only the parts that I worked on.</p>
             </div>
         </div>
     </div>
